fd9ca9000fd19721a6fed4ab2ac522531b930bc6
lulu <lulu@StationEva.local>
2017-01-02 12:17:56 -0500

    add TestGit.java

--- src/practiceCode/ch3/testPack/TestGit.java
+++ src/practiceCode/ch3/testPack/TestGit.java
@@ -4,4 +4,8 @@ package practiceCode.ch3.testPack;
  * Created by lulu on 1/2/17.
  */
 public class TestGit {
+    // main function for test
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
 }
