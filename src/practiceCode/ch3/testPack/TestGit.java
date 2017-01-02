74ffddd280a7a5acc2bf1a3c58a5ab823c7aaebc
pjxiao <flymanxiao@gmail.com>
2017-01-02 12:43:26 -0500

    sync1 from gg

--- src/practiceCode/ch3/testPack/TestGit.java
+++ src/practiceCode/ch3/testPack/TestGit.java
@@ -7,5 +7,6 @@ public class TestGit {
     // main function for test
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("this line is by gg");
     }
 }
